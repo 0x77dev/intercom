239cd7715d7f5c51b03679dc958114c8b8a0a3dc
Mikhail Marynenko <0x77dev@protonmail.com>
2021-01-16 15:53:03 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,72 @@
+# Intercom
+
+Intercom assistant, built for integrate your old-school intercom with Particle Cloud and Home Assistant
+
+## Flow
+
+![Diagram](./diagram.svg)
+
+### Without line voltage change
+
+1. trigger intercom call (call your apartment)
+2. trigger particle function or use home assistant
+3. Line Hangup
+4. Button Press
+5. Button Release
+6. Line Hangdown
+
+### With line voltage change
+
+1. trigger intercom call (call your apartment)
+2. line voltage change (no user interaction needed)
+3. Line Hangup
+4. Button Press
+5. Button Release
+6. Line Hangdown
+
+## Setup
+
+1. [Connect and setup your device with particle cloud](https://setup.particle.io/)
+2. Change MQTT Server IP at `mqtt_server`
+3. add Home Assistant config to `configuration.yaml`
+
+```yaml
+lock:
+  - platform: mqtt
+    name: Intercom
+    state_topic: "homeassistant/intercom/state"
+    command_topic: "homeassistant/intercom/set"
+```
+
+4. [Flash your device](https://docs.particle.io/tutorials/developer-tools/workbench/#cloud-build-and-flash)
+
+## Usage
+
+### Home Assistant
+
+Your entity id will be `lock.intercom`
+
+![Home Assistant Object](./images/homeassistant.png)
+
+### [Particle Cloud Console](https://console.particle.io/devices)
+
+Use functions inside your device view
+
+![Particle Cloud Console Functions](./images/particle_functions.png)
+
+### [Particle Cloud REST function call](https://docs.particle.io/reference/api/)
+
+#### Example Request
+
+Replace `<device_id>` with your id.
+Replace with `<access_token>` with your token.
+
+```console
+$ curl https://api.particle.io/v1/devices/<device_id>/open \
+       -d arg="" \
+       -d access_token=<access_token>
+```
+
+### Siri or Shortcuts
+
+[You can get shortcut here](https://www.icloud.com/shortcuts/ebc8767c61c3402483421795c2679d4e)
---
+++ src/Intercom.ino
@@ -0,0 +1,104 @@
+/*
+ * Project Intercom
+ * Description: Intercom assistant, built for integrate your old-school intercom with Particle Cloud and Home Assistant
+ * Author: Mikhail Marynenko <0x77dev@protonmail.com>
+ */
+
+#include "MQTT.h"
+
+// Hang-up/down relay pin
+int relay1 = D0;
+
+// Open Button relay pin
+int relay2 = D1;
+
+// Intercom line analog pin
+int line = A0;
+
+// check for line voltage change
+bool useLine = false;
+
+byte mqtt_server[] = {192, 168, 31, 164};
+MQTT client(mqtt_server, 1883, mqttCallback);
+
+int open(String command)
+{
+  client.publish("homeassistant/intercom/state", "UNLOCK");
+  digitalWrite(relay1, HIGH); // Hangup
+  Particle.publish("intercom/status", "hangup");
+  delay(1500);                // waits for 1500mS
+  digitalWrite(relay2, HIGH); // press button
+  Particle.publish("intercom/status", "press_button");
+  delay(1350);               // waits for 1350mS
+  digitalWrite(relay2, LOW); // release button
+  Particle.publish("intercom/status", "release_button");
+  delay(1200);               // waits for 1200mS
+  digitalWrite(relay1, LOW); // Hangdown
+  Particle.publish("intercom/status", "hangdown");
+  client.publish("homeassistant/intercom/state", "LOCK");
+  return 1;
+}
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  char p[length + 1];
+  memcpy(p, payload, length);
+  p[length] = NULL;
+
+  if (strcmp(p, "UNLOCK"))
+    open("");
+}
+
+int toggleLine(String command)
+{
+  useLine = !useLine;
+  return 1;
+}
+
+void setup()
+{
+  // Setup Pins
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+
+  // Setup Particle Cloud
+  Particle.function("open", open);
+  Particle.function("toggleLine", toggleLine);
+  Particle.variable("useLine", useLine);
+
+  // Setup MQTT
+  Particle.publish("intercom/mqtt/status", "connecting");
+
+  client.connect("intercom", "mqtt", "mqtt33327113");
+
+  if (client.isConnected())
+  {
+    Serial.print("\nMQTT Connected");
+    Particle.publish("intercom/mqtt/status", "connected");
+    client.publish("homeassistant/intercom/state", "online");
+    client.subscribe("homeassistant/intercom/set");
+  }
+}
+
+void loop()
+{
+  if (useLine)
+  {
+    Particle.publish("intercom/line", String(digitalRead(line)));
+    while (digitalRead(line) == HIGH)
+    {
+      delay(1000);
+      if (digitalRead(line) == HIGH)
+      {
+        Particle.publish("intercom/status", "line_active");
+        open("");
+      }
+      else
+      {
+        Particle.publish("intercom/status", "line_not_active");
+      }
+    }
+    delay(1500);
+    Particle.publish("status", "intercom/standby");
+  }
+}
